ed9232b543a25371d13d2c7fdb402339026f77b2
layonfreitas <lyon.rub@gmail.com.br>
2026-03-20 18:54:31 -0300

    Aula 4 exercicio 3

---
+++ Aula4/Exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+int classe;
+Console.WriteLine("Escolha uma classe: \n 1 - Guerreira \n 2 - Mago \n 3 - Arqueiro");
+classe = int.Parse(Console.ReadLine());
+switch (classe)
+{
+    case 1:
+        Console.WriteLine("Você escolheu a classe Guerreira! Suas habilidades são Ataque Pesado, Defesa Total.");
+        break;
+    case 2:
+        Console.WriteLine("Você escolheu a classe Mago! Suas habilidades são Bola de Fogo, Escudo de Gelo.");
+        break;
+    case 3:
+        Console.WriteLine("Você escolheu a classe Arqueiro! Suas habilidades são Flecha Precisa, Disparo Triplo.");
+        break;
+    default:
+        Console.WriteLine("Opção inválida! Por favor, escolha uma classe válida.");
+        break;
+}
