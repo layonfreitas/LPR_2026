aa070f3cba704a34a0ade83bdac0c5dc0d05e480
layonfreitas <lyon.rub@gmail.com>
2026-03-31 14:08:12 -0300

    aula6 falta terminar o 3

--- Aula6/Exercicio1.c++
+++ Aula6/Exercicio1.c++
@@ -3,40 +3,43 @@
 
 using namespace std;
 
-void inverterPositivo(int numero){
+int inverterPositivo(int numero){
     int invertido = 0;
+
     while (numero > 0) {
         int digito = numero % 10;
         invertido = invertido * 10 + digito;
         numero /= 10;
     }
-    cout << "Número invertido: " << invertido << endl;
+    return invertido;
 }
 
-void inverterNegativo(int numero){
+int inverterNegativo(int numero){
     int invertido = 0;
-    numero = -numero; // Tornar o número positivo para inverter
+    numero = -numero; 
+
     while (numero > 0) {
         int digito = numero % 10;
         invertido = invertido * 10 + digito;
         numero /= 10;
     }
-    cout << "Número invertido: -" << invertido << endl; // Adicionar o sinal negativo de volta
+    return -invertido; 
 }   
 
  
 int main(){
     setlocale(LC_ALL, "pt_BR.utf8");
 
-    int numero;
+    int numero, invertido;
     cout << "Digite um número: ";
     cin >> numero;
+
     if (numero >= 0){
-        inverterPositivo(numero);
+        invertido = inverterPositivo(numero);
     } 
     else {
-        inverterNegativo(numero);
+       invertido = inverterNegativo(numero);
     }
-    
-    
+    cout << "Número invertido: " << invertido << endl;
+
 }
---
+++ Aula6/Exercicio3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static string Nomeheroi1, Nomeheroi2, Nomeheroi3, Nomeheroi4, Nomeheroi5;
+        static string Poderheroi1, Poderheroi2, Poderheroi3, Poderheroi4, Poderheroi5;
+        static int pontuacaoheroi1, pontuacaoheroi2, pontuacaoheroi3, pontuacaoheroi4, pontuacaoheroi5; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma opção:");
+            Console.WriteLine("1 - Cadastrar heróis");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    CadastrarHeroi();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+           
+        }
+
+        static void CadastrarHeroi(){
+            Console.WriteLine("Digite o nome do herói 1:");
+            Nomeheroi1 = Console.ReadLine();
+            Console.WriteLine("Digite o poder do herói 1:");
+            Poderheroi1 = Console.ReadLine();
+            Console.WriteLine("Digite a pontuação do herói 1:");
+            pontuacaoheroi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do herói 2:");
+            Nomeheroi2 = Console.ReadLine();    
+            Console.WriteLine("Digite o poder do herói 2:");
+            Poderheroi2 = Console.ReadLine();
+            Console.WriteLine("Digite a pontuação do herói 2:");
+            pontuacaoheroi2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do herói 3:");
+            Nomeheroi3 = Console.ReadLine();    
+            Console.WriteLine("Digite o poder do herói 3:");
+            Poderheroi3 = Console.ReadLine();
+            Console.WriteLine("Digite a pontuação do herói 3:");
+            pontuacaoheroi3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do herói 4:");
+            Nomeheroi4 = Console.ReadLine();    
+            Console.WriteLine("Digite o poder do herói 4:");    
+            Poderheroi4 = Console.ReadLine();
+            Console.WriteLine("Digite a pontuação do herói 4:");
+            pontuacaoheroi4 = int.Parse(Console.ReadLine());       
+
+            Console.WriteLine("Digite o nome do herói 5:");
+            Nomeheroi5 = Console.ReadLine();    
+            Console.WriteLine("Digite o poder do herói 5:");
+            Poderheroi5 = Console.ReadLine();
+            Console.WriteLine("Digite a pontuação do herói 5:");
+            pontuacaoheroi5 = int.Parse(Console .ReadLine());   
+
+        }
+
+       
+        }
+    }
