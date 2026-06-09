92dbac53c35249190b6cf690e66110a2ab78e1fe
layonfreitas <lyon.rub@gmail.com>
2026-06-09 14:02:01 -0300

    terminando a aula 7

---
+++ Aula7/Exercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int soma = 0;
+
+            for (int i = 0; i <3; i++)
+            {
+                for (int j = 0; j <3; j++)
+                {
+                    Console.WriteLine($"Digite o termo da linha {i} e da coluna {j}:");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i <3; i++)
+            {
+                for (int j = 0; j <3; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                   soma += matriz[i, j];
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"A soma de todos os termos da matriz é: {soma}");
+        }
+    }
+}
---
+++ Aula7/Exercicio6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] distancia = {{0, 524, 521, 882}, {524, 0, 434, 586}, {521, 434, 0, 429}, {882, 586, 429, 0}};
+            
+
+            string cidade1, cidade2;
+            cidade1 = "1";
+            cidade2 = "2";
+
+            while (cidade1 != cidade2)
+            {
+
+                Console.WriteLine("digite a cidade de origem:");
+                cidade1 = Console.ReadLine();
+                Console.WriteLine("digite a cidade de destino:");
+                cidade2 = Console.ReadLine();
+
+                if (cidade1 == cidade2)
+                {
+                    Console.WriteLine("Voce saiu");
+                }
+                else
+                {
+                    MostrarDistancia(cidade1, cidade2, distancia);
+                }
+
+        }   
+    }
+    static void MostrarDistancia(string cidade1, string cidade2, int[,] distancia){
+    if ((cidade1 == "Vitória" && cidade2 == "Belo Horizonte") || (cidade1 == "Belo Horizonte" && cidade2 == "Vitória"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[0, 1]} km");
+    }
+    else if ((cidade1 == "Vitória" && cidade2 == "Rio de Janeiro") || (cidade1 == "Rio de Janeiro" && cidade2 == "Vitória"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[0, 2]} km");
+    }
+    else if ((cidade1 == "Vitória" && cidade2 == "São Paulo") || (cidade1 == "São Paulo" && cidade2 == "Vitória"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[0, 3]} km");
+    }
+    else if ((cidade1 == "Belo Horizonte" && cidade2 == "Rio de Janeiro") || (cidade1 == "Rio de Janeiro" && cidade2 == "Belo Horizonte"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[1, 2]} km");
+    }
+    else if ((cidade1 == "Belo Horizonte" && cidade2 == "São Paulo") || (cidade1 == "São Paulo" && cidade2 == "Belo Horizonte"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[1, 3]} km");
+    }
+    else if ((cidade1 == "Rio de Janeiro" && cidade2 == "São Paulo") || (cidade1 == "São Paulo" && cidade2 == "Rio de Janeiro"))
+    {
+        Console.WriteLine($"A distancia entre {cidade1} e {cidade2} é {distancia[2, 3]} km");
+    }
+    else
+    {
+        Console.WriteLine("Cidade não encontrada");
+    }
+}
+}}
